2f39bdc562c05bfbe301bda58df4b2a691e56f62
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-03-06 12:35:00 -0500

    misc: Increase coarse sim duration

--- main.py
+++ main.py
@@ -429,7 +429,7 @@ def make_exp_params(study_name: str) -> List[ExpParam]:
         'Ecov': ECOV, 'Ebod': EBOD,
         'vcov': 1, 'mcov': 0.0,
         'psub': 600*10,
-        'dt': 5e-5, 'tf': 0.2,
+        'dt': 5e-5, 'tf': 0.25,
         'ModifyEffect': '',
         'SwellingDistribution': 'uniform'
     })
